9c747c53080ee2ec5a44eeab015069ede8197a2d
Nada Gohider <nadagohider@gmail.com>
2019-07-09 20:55:47 -0400

    Delete analyze.py

--- bnb/analyze.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def group_by_date(dtafrm):
-    
-    '''accepts a dataframe, groups data by check_in date and returns the average price for each group '''
-    
-    return(pd.Series(data= dtafrm.groupby('CheckIn')['Price'].mean()))
